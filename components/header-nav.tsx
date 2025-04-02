48be8b8acd6e828819bb0e1851565872a97aab9a
adibfiroz <adibfiroz9702@gmail.com>
2025-04-02 15:32:43 +0530

    framer and eslint

--- components/header-nav.tsx
+++ components/header-nav.tsx
@@ -1,6 +1,9 @@
+"use client"
+
 import { ChevronRight } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
+import { motion } from "framer-motion"
 
 interface HeaderNavProps {
     title: string
@@ -14,7 +17,12 @@ const HeaderNav = ({
     return (
         <div className="bg-[url('/header-bg.png')] flex items-center h-[180px] md:h-[250px]">
             <div className='flex justify-between flex-wrap container mx-auto p-4'>
-                <h2 className='text-3xl lg:text-6xl font-bold text-[#141414]'>{title}</h2>
+                <motion.h2
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className='text-3xl lg:text-6xl font-bold text-[#141414]'>{title}
+                </motion.h2>
                 <ul className='flex items-center gap-2 text-lg'>
                     <li className='text-[#74787C]'>
                         <Link href="/">Home</Link>
